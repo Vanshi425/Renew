928d7bdc0745b6327161711b794a0717db7ade2e
Vanshi425 <vanshikaagarwal7010@gmail.com>
2026-02-04 16:29:13 +0530

    fix allowed hosts for render

--- Renew/settings.py
+++ Renew/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8*_ca(nt+le(wj(rv(we)3(2$&#5u9x98^mh^!$jppsrxje9c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['renew-1-zdav.onrender.com','local host','127.0.0.1']
 
 
 # Application definition
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Renew.urls'
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
